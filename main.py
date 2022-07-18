02edca066056ff04b6fc27c4d059543503dc4fe9
Andy Zhao <andyzhao21@gmail.com>
2022-07-18 03:23:41 -0400

    Updated About Page

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 from riotwatcher import LolWatcher, ApiError
 import streamlit as st
 
-lol_watcher = LolWatcher(getKey())
+lol_watcher = LolWatcher("RGAPI-c7a2c25c-fd71-4231-8505-c78d66b30287")
 
 st.set_page_config(
     page_title="League Stats",
