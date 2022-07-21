592153f37f2e8d66e508c585cd16433b149dfa37
Andy Zhao <andyzhao21@gmail.com>
2022-07-21 13:22:36 -0400

    Utilities for esports query

--- utilities.py
+++ utilities.py
@@ -49,6 +49,43 @@ def valorantRegionIdentifier(region):
         case "North America":
             return "na"
 
-
-
-
+def orgIdentifier(org):
+    match org:
+        case "Version 1" | "V1":
+            return "version1"
+        case "Cloud 9":
+            return "c9"
+        case "Tsm" | "Team Solo Mid" | "tsm":
+            return "TSM"
+        case "Optic" | "Optic Gaming" | "optic":
+            return "OpTic_Gaming"
+        case "Loud" | "loud":
+            return "LOUD"
+        case "Guild" | "guild":
+            return "Guild Esports"
+        case "Fpx" | "Fun Plus Phoenix" "fun plus phoenix" | "fpx":
+            return "FunPlus Phoenix"
+        case "Leviatan" | "leviatan":
+            return "Leviatán"
+        case "Xset" | "xset":
+            return "XSET"
+        case "Kru" | "KRU Esports" | "kru" | "kru esports":
+            return "KRÜ_Esports"
+        case "Tl" | "tl" | "team liquid":
+            return "Team Liquid"
+        case "100T" | "100t" | "100 thieves":
+            return "100 Thieves"
+        case "Zeta" | "Zeta Division":
+            return "ZETA_DIVISION"
+        case "Faze" | "Faze clan" | "Faze Clan":
+            return "FaZe Clan"
+        case "Imt":
+            return "Immortals"
+        case "Nyfu":
+            return "NYFU"
+        case "Darkzero Esports" | "Darkzero":
+            return "DarkZero Esports"
+        case "Geng" | "Gen.g" | "Geng Esports":
+            return "Gen.G Esports"
+        case _:
+            return None
